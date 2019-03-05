f106c3b9df445818a7f64b91b0c5e13fff94bf6d
Jordan Williams <Oryinn@Jordans-MacBook-Pro-6.local>
2019-03-05 16:09:19 -0500

    Application routing works properly

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -1,14 +1,14 @@
-const {Accessory} = require("../models/Accessory.js")
-const {Apparel} = require("../Models/Apparel.js")
+const Accessory = require("../models/Accessory.js")
+const Apparel = require("../Models/Apparel.js")
 
 const applicationController = {
     index: (req, res) => {
-        res.send('test, connected')
-        // Apparel.find().then(apparel => {
-        // Accessory.find().then(accessory => {
-        //     res.render('index', {apparel, accessory})
-        // })
-    // })
+        // res.send('test, connected')
+        Apparel.find().then(apparel => {
+        Accessory.find().then(accessory => {
+            res.render('index', {apparel, accessory})
+        })
+    })
     }
 }
 
