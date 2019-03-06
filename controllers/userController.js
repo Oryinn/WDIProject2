83abc1f5e3e47453deb724a7fa24c956376b2546
Jordan Williams <Oryinn@Jordans-MacBook-Pro-6.local>
2019-03-06 13:32:58 -0500

    Apparently some files didnt get staged? who knows

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,15 @@
 const User = require('../Models/User')
+const Apparel = require("../Models/Apparel.js")
+const Accessory = require("../Models/Accessory.js")
 
 const userController = {
     index: (req, res) => {
-
+        // res.send('test, connected')
+        Apparel.find().then(apparel => {
+        Accessory.find().then(accessory => {
+            res.render('admin/index', {apparel, accessory})
+        })
+    })
     }
 }
 
